2233fbe0fdde6e8d3ce45e699a4624ba0dfa678e
comverser <hyunduk0206@gmail.com>
2022-04-13 06:06:33 +0000

    first commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import anime from 'animejs/lib/anime.es';
+	import { onMount } from 'svelte';
+	onMount(() => {
+		// JS 코드 시작
+		anime({
+			targets: '.el',
+			translateX: 270
+		});
+		// JS 코드 끝
+	});
+</script>
+
+<div>
+	<!-- HTML 코드 시작 -->
+	<div class="el">test</div>
+	<!-- HTML 코드 끝 -->
+</div>
+
+<style>
+	/* CSS 코드 시작 */
+	div {
+		background-color: black;
+		display: inline;
+	}
+	/* CSS 코드 끝*/
+</style>
